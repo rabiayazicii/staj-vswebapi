a8f3d10032dbfafa41ed7d2afa9dfb6895049a5c
Rabia Yazıcı <rabiayazici@Rabia-MacBook-Air-2.local>
2024-07-26 14:39:01 +0300

    first commit

---
+++ ConsumeWebAPI/Controllers/NYOController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeWebAPI.Controllers
+{
+	public class NYOController:Controller
+	{
+		Uri baseAdsress = new Uri("https://localhost:7117/api");
+		private readonly HttpClient _client;
+
+		public NYOController()
+		{
+			_client = new HttpClient();
+			_client.BaseAddress = baseAdsress;
+		}
+
+        public IActionResult Index()
+		{
+			List<NYOViewModel> list = new List<NYOViewModel>();
+			HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/product/Get").Result;
+
+			if (response.IsSuccessStatusCode)
+			{
+				string data = response.Content.ReadAsStringAsync().Result;
+				list = JsonConvert.DeserializeObject<List<NYOViewModel>>(data);
+			}
+			return View(list);
+		}
+	}
+}
+
---
+++ ConsumeWebAPI/Models/NYOViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWebAPI.Models
+{
+	public class NYOViewModel
+	{
+        [Key]
+        public int ODEMENO { get; set; }
+        [Required]
+        [DisplayName("TC KIMLIK NO")]
+        public string TCKIMLIKNO { get; set; }
+        public string MUSTERIAD { get; set; }
+        public string MUSTERISOYAD { get; set; }
+        public int ODEME_KD { get; set; }
+        public int ODEME_TTR { get; set; }
+        public DateTime ODEME_TR { get; set; }
+        public string ODEME_ACK { get; set; }
+    }
+}
+
